798d136ecc0c61076865e32b78e8d46148b712e6
Fasle00 <94972761+Fasle00@users.noreply.github.com>
2022-12-14 09:51:41 +0100

    added files

--- src/fabian.java
+++ src/fabian.java
@@ -1,2 +1,28 @@
 public class fabian {
+    public static void validChar(){
+        System.out.println("hej");
+    }
 }
+/*
+void showProgress()
+
+linus:
+void showHiddenWord()
+
+fabian:
+boolean validChar()
+
+
+void main-main(String[] args){
+ord = något();
+for(int i = 0; i < ord.length(); i++){
+hiddenword = hiddenword + "_";
+}
+    |
+  __|_
+ /    \
+/      \
+guessed letters: a b c
+____
+guess a letter: (guess)
+ */
