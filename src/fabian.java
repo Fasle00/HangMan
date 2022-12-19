9b37c9307497442395c5eb8396d0a264a8c66c2f
Fasle00 <94972761+Fasle00@users.noreply.github.com>
2022-12-19 12:48:13 +0100

    added files

--- src/fabian.java
+++ src/fabian.java
@@ -1,15 +1,113 @@
 public class fabian {
-    public static void validChar(){
-        System.out.println("hej");
+    public static String usedChars = "";
+    public static boolean validChar(char in){
+        if (usedChars.contains(String.valueOf(in))){
+            return false;
+        }else {
+            usedChars += in;
+            return true;
+        }
+    }
+    public static void showProgress(int fel){
+        if (fel == 11){
+            System.out.println("       _________   ");
+            System.out.println("       |/      ¤   ");
+            System.out.println("       |      /|\\ ");
+            System.out.println("       |      / \\  ");
+            System.out.println("  ____/|\\___       ");
+            System.out.println(" /          \\     ");
+            System.out.println("/            \\    ");
+        }else if (fel == 10){
+            System.out.println("       _________   ");
+            System.out.println("       |/      ¤   ");
+            System.out.println("       |      /|\\ ");
+            System.out.println("       |      /    ");
+            System.out.println("  ____/|\\___       ");
+            System.out.println(" /          \\     ");
+            System.out.println("/            \\    ");
+        }else if (fel == 9){
+            System.out.println("       _________   ");
+            System.out.println("       |/      ¤   ");
+            System.out.println("       |      /|\\ ");
+            System.out.println("       |           ");
+            System.out.println("  ____/|\\___       ");
+            System.out.println(" /          \\     ");
+            System.out.println("/            \\    ");
+        }else if (fel == 8){
+            System.out.println("       _________   ");
+            System.out.println("       |/      ¤   ");
+            System.out.println("       |      /|   ");
+            System.out.println("       |           ");
+            System.out.println("  ____/|\\___      ");
+            System.out.println(" /          \\     ");
+            System.out.println("/            \\    ");
+        }else if (fel == 7){
+            System.out.println("       _________   ");
+            System.out.println("       |/      ¤   ");
+            System.out.println("       |       |   ");
+            System.out.println("       |           ");
+            System.out.println("  ____/|\\___       ");
+            System.out.println(" /          \\     ");
+            System.out.println("/            \\    ");
+        }else if (fel == 6){
+            System.out.println("       _________   ");
+            System.out.println("       |/      ¤   ");
+            System.out.println("       |           ");
+            System.out.println("       |           ");
+            System.out.println("  ____/|\\___       ");
+            System.out.println(" /          \\     ");
+            System.out.println("/            \\    ");
+        }else if (fel == 5){
+            System.out.println("       _________   ");
+            System.out.println("       |/          ");
+            System.out.println("       |           ");
+            System.out.println("       |           ");
+            System.out.println("  ____/|\\___       ");
+            System.out.println(" /          \\     ");
+            System.out.println("/            \\    ");
+        }else if (fel == 4){
+            System.out.println("       _________   ");
+            System.out.println("       |           ");
+            System.out.println("       |           ");
+            System.out.println("       |           ");
+            System.out.println("  ____/|____       ");
+            System.out.println(" /          \\     ");
+            System.out.println("/            \\    ");
+        }else if (fel == 3){
+            System.out.println("       _________   ");
+            System.out.println("       |           ");
+            System.out.println("       |           ");
+            System.out.println("       |           ");
+            System.out.println("  _____|____       ");
+            System.out.println(" /          \\     ");
+            System.out.println("/            \\    ");
+        }else if (fel == 2){
+            System.out.println("                   ");
+            System.out.println("       |           ");
+            System.out.println("       |           ");
+            System.out.println("       |           ");
+            System.out.println("  _____|____       ");
+            System.out.println(" /          \\     ");
+            System.out.println("/            \\    ");
+        }else if (fel == 1){
+            System.out.println("                   ");
+            System.out.println("                   ");
+            System.out.println("                   ");
+            System.out.println("                   ");
+            System.out.println("  __________       ");
+            System.out.println(" /          \\     ");
+            System.out.println("/            \\    ");
+        }
     }
 }
 /*
-void showProgress()
+
 
 linus:
 void showHiddenWord()
 
 fabian:
+void showProgress()
 boolean validChar()
 
 
