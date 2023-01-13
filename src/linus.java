ecf56f073cdc7ae89f51b13a2c5367df07b8bf05
L1nu5-21 <94972443+L1nu5-21@users.noreply.github.com>
2023-01-13 11:12:40 +0100

    :)

--- src/linus.java
+++ src/linus.java
@@ -1,2 +1,11 @@
 public class linus {
+    public static void showHiddenWord() {
+        System.out.println(Main.hiddenWord);
+    }
+
+    public static void underScores() {
+        for (int i = 0; i < Main.hiddenWord.length(); i++){
+
+        }
+    }
 }
